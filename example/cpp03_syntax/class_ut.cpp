58e1597aeb90d62f5c4ca81b95b4c39578136fd9
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2025-09-24 09:16:48 +0900

    small fixed

--- example/cpp03_syntax/class_ut.cpp
+++ example/cpp03_syntax/class_ut.cpp
@@ -208,7 +208,7 @@ struct Person {
     uint32_t weight_kg;   // 体重 (kg単位と仮定)
     uint32_t calc_bmi();  // メンバ関数
 
-    char const* get_first_name();
+    char const* get_full_name();
 };
 
 // コンストラクタの定義
@@ -228,17 +228,16 @@ Person::Person(char const* family_name, char const* first_name, uint32_t height_
     // @@@ ignore end
 }
 
-// 新規関数
-char const* Person::get_first_name()
+char* make_first_name(char const* family_name, char const* first_name)
 {
-    if (full_name) {
+    if (!family_name || !first_name) {
         return NULL;
     }
 
     size_t family_len = strlen(family_name);
     size_t first_len  = strlen(first_name);
 
-    full_name = (char*)malloc(family_len + 1 + first_len + 1);  // 姓と名の間に1スペース入れる
+    char* full_name = (char*)malloc(family_len + 1 + first_len + 1);  // 姓と名の間に1スペース入れる
 
     if (!full_name) {
         return NULL;
@@ -250,6 +249,16 @@ char const* Person::get_first_name()
 
     return full_name;
 }
+
+// 新規関数
+char const* Person::get_full_name()
+{
+    if (full_name) {  // すでにfull_nameの生成済
+        return full_name;
+    }
+
+    return full_name = make_first_name(family_name, first_name);
+}
 // @@@ sample end
 
 TEST(cpp03, class_exp3)
@@ -259,7 +268,7 @@ TEST(cpp03, class_exp3)
 
     Person person("yamada", "taro", 173, 75);  // オブジェクトの生成
 
-    char const* full_name = person.get_first_name();
+    char const* full_name = person.get_full_name();
 
     ASSERT_STREQ(full_name, "yamada taro");  // 文字列として同値
     // @@@ sample end
@@ -299,7 +308,7 @@ struct Person {
     uint32_t weight_kg;   // 体重 (kg単位と仮定)
     uint32_t calc_bmi();  // メンバ関数
 
-    char const* get_first_name();
+    char const* get_full_name();
 
     ~Person();  // デストラクタ
 };
@@ -319,16 +328,16 @@ Person::Person(char const* family_name, char const* first_name, uint32_t height_
     // @@@ ignore end
 }
 
-char const* Person::get_first_name()
+char* make_first_name(char const* family_name, char const* first_name)
 {
-    if (full_name) {
+    if (!family_name || !first_name) {
         return NULL;
     }
 
     size_t family_len = strlen(family_name);
     size_t first_len  = strlen(first_name);
 
-    full_name = (char*)malloc(family_len + 1 + first_len + 1);  // 姓と名の間に1スペース入れる
+    char* full_name = (char*)malloc(family_len + 1 + first_len + 1);  // 姓と名の間に1スペース入れる
 
     if (!full_name) {
         return NULL;
@@ -341,17 +350,17 @@ char const* Person::get_first_name()
     return full_name;
 }
 
+char const* Person::get_full_name()
+{
+    if (full_name) {  // すでにfull_nameの生成済
+        return full_name;
+    }
+
+    return full_name = make_first_name(family_name, first_name);
+}
+
 Person::~Person()  // デストラクタの定義。
 {
-    // @@@ ignore begin
-    if (freed) {  // 編集の都合上、このif文はデストラクタの説明に不要なので、このコードはignoreする
-        return;
-    }
-    else {
-        freed = true;
-    }
-    // @@@ ignore end
-
     if (full_name) {
         free((void*)full_name);
     }
@@ -377,7 +386,7 @@ TEST(cpp03, class_exp4)
     {
         Person person("yamada", "taro", 173, 75);  // オブジェクトの生成
 
-        char const* full_name = person.get_first_name();
+        char const* full_name = person.get_full_name();
 
         ASSERT_STREQ(full_name, "yamada taro");  // 文字列として同値
     }  // この行でpersonがスコープアウトするためpersonのデストラクタが呼ばれ、
@@ -387,3 +396,110 @@ TEST(cpp03, class_exp4)
 }
 SUPPRESS_WARN_END;
 }  // namespace cpp_style_4
+
+namespace cpp_style_5 {
+SUPPRESS_WARN_BEGIN
+SUPPRESS_WARN_GCC_NOT_EFF_CPP;
+bool freed = false;
+// @@@ sample begin 5:0
+
+class Person {
+public:
+    Person(char const* family_name, char const* first_name, uint32_t height_cm,
+           uint32_t weight_kg);  // コンストラクタ
+
+    uint32_t    calc_bmi();  // メンバ関数
+    char const* get_full_name();
+
+    ~Person();  // デストラクタ
+
+private:  // メンバ変数への外部からのアクセスを禁止する。
+    char const* family_name;
+    char const* first_name;
+    char*       full_name;  // 新規メンバ
+
+    uint32_t height_cm;  // 身長 (cm単位と仮定)
+    uint32_t weight_kg;  // 体重 (kg単位と仮定)
+};
+
+Person::Person(char const* family_name, char const* first_name, uint32_t height_cm,
+               uint32_t weight_kg)
+    // clang-format off
+    : family_name(family_name), first_name(first_name),
+      full_name(NULL),          // 新規に追加されたメンバをNULLで初期化
+      height_cm(height_cm), weight_kg(weight_kg)  // clang-format on
+{
+    // コンストラクタの中身は省略
+    // @@@ ignore begin
+    // メンバ変数の初期化などの処理はここに通常の関数の中に入れることができる
+    assert(family_name && first_name);  // 本来ならエクセプションの送出すべきだが
+    assert(height_cm != 0);             // 本来ならエクセプションの送出すべきだが
+    // @@@ ignore end
+}
+
+char* make_first_name(char const* family_name, char const* first_name)
+{
+    // 関数の中身は変わっていないので省略
+    // @@@ ignore begin
+    if (!family_name || !first_name) {
+        return NULL;
+    }
+
+    size_t family_len = strlen(family_name);
+    size_t first_len  = strlen(first_name);
+
+    char* full_name = (char*)malloc(family_len + 1 + first_len + 1);  // 姓と名の間に1スペース入れる
+
+    if (!full_name) {
+        return NULL;
+    }
+
+    strcpy(full_name, family_name);
+    full_name[family_len] = ' ';
+    strcpy(full_name + family_len + 1, first_name);
+
+    return full_name;
+    // @@@ ignore end
+}
+
+char const* Person::get_full_name()
+{
+    if (full_name) {  // すでにfull_nameの生成済
+        return full_name;
+    }
+
+    return full_name = make_first_name(family_name, first_name);
+}
+
+Person::~Person()  // デストラクタの定義。
+{
+    if (full_name) {
+        free((void*)full_name);
+    }
+}
+// @@@ sample end
+
+uint32_t Person::calc_bmi()  // Personのcalc_bmi()の定義
+{
+    // BMI = 体重(kg) / (身長(m))^2
+    // 身長をcmからmに変換: height_cm / 100
+    // BMI = weight_kg / ((height_cm / 100.0)^2)
+    // 整数演算で行うため: BMI = (weight_kg * 10000) / (height_cm^2)
+
+    uint32_t bmi = weight_kg * 10000 / (height_cm * height_cm);
+
+    return bmi;
+}
+TEST(cpp03, class_exp4)
+{
+    // @@@ sample begin 5:1
+
+    Person person("yamada", "taro", 173, 75);  // オブジェクトの生成
+
+    char const* full_name = person.get_full_name();
+
+    ASSERT_STREQ(full_name, "yamada taro");  // 文字列として同値
+    // @@@ sample end
+}
+SUPPRESS_WARN_END;
+}  // namespace cpp_style_5
